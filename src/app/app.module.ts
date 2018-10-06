26a0147056bbbb54d1e66ab3fd9622a074e41eec
DESKTOP-5H6PU6V\_caro <caro.frontend@gmail.com>
2018-10-06 18:52:58 -0300

    iconos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,39 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 
+//material
+import { CdkTableModule } from '@angular/cdk/table';
+import { CdkTreeModule } from '@angular/cdk/tree';
+import {
+  MAT_DIALOG_DEFAULT_OPTIONS,
+  MatSlideToggleModule, MatButtonToggleModule, MatCardModule, MatButtonModule, MatToolbarModule,
+  MatAutocompleteModule, MatCheckboxModule, MatIconModule, MatMenuModule, MatSelectModule,
+  MatFormFieldModule, MatInputModule, MatDatepickerModule, MatRadioModule, MatSliderModule,
+  MatTabsModule, MatGridListModule, MatSnackBarModule, MatSidenavModule, MatChipsModule,
+  MatDialogModule, MatDividerModule, MatExpansionModule, MatListModule, MatNativeDateModule,
+  MatPaginatorModule, MatProgressBarModule, MatProgressSpinnerModule, MatRippleModule,
+  MatSortModule, MatStepperModule, MatTableModule, MatTooltipModule, MatMenuTrigger,
+  MatPaginatorIntl, MatBadgeModule, MatBottomSheetModule, MatTreeModule, ErrorStateMatcher, ShowOnDirtyErrorStateMatcher,
+} from '@angular/material';
+
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CdkTableModule, CdkTreeModule,
+    MatChipsModule, MatDialogModule, MatDividerModule, MatTooltipModule,
+    MatExpansionModule, MatListModule, MatNativeDateModule,
+    MatPaginatorModule, MatProgressBarModule, MatProgressSpinnerModule,
+    MatRippleModule, MatSortModule, MatStepperModule, MatTableModule,
+    MatGridListModule, MatSnackBarModule, MatSidenavModule,
+    MatFormFieldModule, MatInputModule, MatButtonToggleModule, MatSlideToggleModule,
+    MatButtonModule, MatCardModule, MatToolbarModule, MatAutocompleteModule,
+    MatCheckboxModule, MatIconModule, MatMenuModule, MatSelectModule, MatDatepickerModule,
+    MatRadioModule, MatSliderModule, MatTabsModule, MatBadgeModule, MatBottomSheetModule,
+    MatTreeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
