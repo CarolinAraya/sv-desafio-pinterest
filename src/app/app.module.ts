9f463a4c06132904ae24e058886e132d41745429
DESKTOP-5H6PU6V\_caro <caro.frontend@gmail.com>
2018-10-08 13:28:44 -0300

    funcionando cúsqueda con botón

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { HttpModule} from '@angular/http'
+import { HttpModule} from '@angular/http';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
@@ -48,7 +49,8 @@ import {
     MatCheckboxModule, MatIconModule, MatMenuModule, MatSelectModule, MatDatepickerModule,
     MatRadioModule, MatSliderModule, MatTabsModule, MatBadgeModule, MatBottomSheetModule,
     MatTreeModule,
-    HttpModule
+    HttpModule,
+    FormsModule
 
   ],
   entryComponents: [
